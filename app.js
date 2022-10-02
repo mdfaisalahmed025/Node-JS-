7161820b8e562ccbeadfcabf0a9a101e0360e464
Md Faisal Ahmed <71064436+mdfaisalahmed025@users.noreply.github.com>
2022-10-02 17:34:39 +0600

    Node Js  practices

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
+const {createReadStream} = require('fs')
+const stream =createReadStream('./content/big.txt');
 
-const names = require('./4name')
-const sayhi= require('./5utils')
-const data = require('./alter')
-require('./addvalues')
-
-console.log(data)
-
-sayhi('susan')
-sayhi(names.jhon)
-sayhi(names.peter)
+stream.on( 'data', (result) => {
+    console.log(result)
+})
